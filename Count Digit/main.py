95348e7e94cece78f374129b1654b397b5a7b4e3
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-06-10 13:35:53 +0530

    count digit in python

---
+++ Count Digit/main.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a Number: "))
+
+def countDigit(n):
+    if(n == 0):
+        return 1
+    n = abs(n)
+    count = 0
+    while n > 0:
+        n //= 10
+        count += 1
+    return count
+
+result = countDigit(n)
+print(result)
