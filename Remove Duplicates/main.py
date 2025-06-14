b9b968266f7c5848dc604d08b5d0085ffdf135e7
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-06-14 10:24:06 +0530

    remove duplicates in python

--- Remove Duplicates/main.py
+++ Remove Duplicates/main.py
@@ -1,10 +1,10 @@
-class Solution:
-    def removeDuplicates(self, nums:list[int]) -> int:
-        if not nums:
-            return 0
-        x = 0
-        for i in range(1 , len(nums)):
-            if nums[i] > nums[x]:
-                x += 1
-                nums[x] = nums[i]
-        return x + 1
+def removeDuplicates(nums):
+    for i in range(len(nums)):
+        x =0
+        if nums[i] > nums[x]:
+            nums[x] = nums[i]
+            x += 1
+    return x + 1
+
+print(removeDuplicates([1, 1, 2]))  # Output: 2
+
