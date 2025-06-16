b7bff240215a87d9f6a07fe96de0ddc2797ceeec
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-06-16 13:30:59 +0530

    Stock buy and sell in python

---
+++ Best Time to Buy and Sell Stock/main.py
@@ -0,0 +1,12 @@
+def maxProfit(prices):
+    min = prices[0]
+    maxProfit = 0
+
+    for i in range(len(prices)):
+        if prices[i] - min > maxProfit:
+            maxProfit = prices[i] - min
+        if prices[i] < min:
+            min = prices[i]
+    return maxProfit
+
+print(maxProfit([7,1,5,3,6,4]))
