d731a0b62b49c52e5401e11823afdf795a14ace1
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-06-16 13:53:17 +0530

    Move zero in .js and .py

---
+++ Move Zero/index.js
@@ -0,0 +1,17 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+var moveZeroes = function(nums) {
+    let x = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            nums[x] = nums[i]
+            x++
+        }
+    }
+
+    for(let i =x; i < nums.length; i++) {
+        nums[i] = 0
+    }
+};
