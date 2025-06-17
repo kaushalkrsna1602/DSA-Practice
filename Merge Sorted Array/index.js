6e7cc898626970388bc1973ac06037ec1538e927
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-06-17 15:14:17 +0530

    Appraoch 3 mergeArray

--- Merge Sorted Array/index.js
+++ Merge Sorted Array/index.js
@@ -4,6 +4,22 @@
 
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
+// Approach1
+
+let nums1 = [2, 5, 6]
+let nums2 = [3, 7, 9]
+
+let m = nums1.length;
+let n = nums2.length
+
+function mergeArray1(nums1 , m , nums2 , n) {
+    nums1 = [...nums1 , ...nums2]
+    console.log(nums1.sort((a , b) => a - b))
+}
+
+mergeArray1(nums1 , m , nums2 , n)
+
+// Approach2
 function mergeSortedArray(nums1 , m , nums2, n) {
     let nums3 = nums1.slice(0,m)
     let p1 = 0;
@@ -20,3 +36,23 @@ function mergeSortedArray(nums1 , m , nums2, n) {
         }
     }
 }
+
+
+// Approach 3
+
+ var merge = function(nums1, m, nums2, n) {
+    let p1 = m - 1;
+    let p2 = n - 1;
+
+    for (let i =  m + n - 1; i >=0; i--) {
+        if (p2 < 0) break;
+
+        if (p1 >=0 && nums1[p1] > nums2[p2]) {
+            nums1[i] = nums1[p1]
+            p1--
+        } else {
+            nums1[i] = nums2[p2];
+            p2--
+        }
+    }
+};
--- Merge Sorted Array/main.py
+++ Merge Sorted Array/main.py
@@ -1,3 +1,19 @@
+# Approach 1
+
+nums1 = [2, 5, 6]
+nums2 = [3, 7, 9]
+m = len(nums1)
+n = len(nums2)
+
+def merge_array1(nums1, m, nums2, n):
+    nums1 = nums1 + nums2
+    nums1.sort()
+    print(nums1)
+
+merge_array1(nums1 , nums2 , m , n)
+
+
+# Approach 2 
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
@@ -21,3 +37,21 @@ class Solution:
             nums1[p] = nums2[p2]
             p2 -= 1
             p -= 1
+
+
+# Approach 3
+
+def merge(nums1, m, nums2, n):
+    p1 = m - 1
+    p2 = n - 1
+
+    for i in range(m + n - 1, -1, -1):
+        if p2 < 0:
+            break
+        
+        if p1 >= 0 and nums1[p1] > nums2[p2]:
+            nums1[i] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[i] = nums2[p2]
+            p2 -= 1
