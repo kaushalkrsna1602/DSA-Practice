36a2df328eb8c71d9d8e1d4f0e115016ef00bd0c
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-06-19 12:04:34 +0530

    brute force single number in js

---
+++ Single Number/index.js
@@ -0,0 +1,28 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Approach 1
+
+var singleNumber = function(nums) {
+    let hash = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!hash[nums[i]]) {
+            hash[nums[i]] = 1
+        } else {
+            hash[nums[i]] ++
+        }
+    }
+
+    // console.log(hash)
+
+    for (let i = 0; i < nums.length; i++) {
+        if(hash[nums[i]] == 1) {
+            return nums[i]
+        }
+    }
+};
+
+console.log(singleNumber([1, 2 , 3, 2 , 3]))
+
