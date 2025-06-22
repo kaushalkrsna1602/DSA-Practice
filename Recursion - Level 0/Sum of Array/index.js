3829be4b5d60a1045df3bd309ee68a5543a127b6
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-06-22 08:58:15 +0530

    sum of all odd numbers in an Array [js]

--- Recursion - Level 0/Sum of Array/index.js
+++ Recursion - Level 0/Sum of Array/index.js
@@ -2,7 +2,7 @@
 
 let arr = [2,4,5,9,7,3,4]
 
-console.log(arrSum(arr.length - 1))
+// console.log(arrSum(arr.length - 1))
 
 function arrSum(n) {
     if (n == 0) return arr[0]
@@ -10,3 +10,15 @@ function arrSum(n) {
 }
 
 
+
+
+// Sum of all odd Numbers in an Array
+
+function oddSum(n) {
+    let isOdd = (arr[n] % 2) != 0;
+    if(n == 0) {
+        return isOdd ? arr[n] : 0 }
+    return (isOdd ? arr[n] : 0) + oddSum(n - 1)
+}
+
+console.log(oddSum(arr.length - 1))
