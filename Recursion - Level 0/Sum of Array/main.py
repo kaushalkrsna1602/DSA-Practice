5ed78feb693401004b6ec3350a401670d3319a0f
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-06-22 09:20:22 +0530

    sum of all odd numbers in an Array [ py ]

--- Recursion - Level 0/Sum of Array/main.py
+++ Recursion - Level 0/Sum of Array/main.py
@@ -6,4 +6,14 @@ def sum(n):
     if n == 0: return list[0]
     return list[n] + sum(n - 1)
 
-print(sum(len(list) - 1))
+# print(sum(len(list) - 1))
+
+# Sum of all odd Numbers in an Array
+
+def oddSum(n):
+    isOdd = (list[n] % 2) != 0
+    if n == 0:
+        return list[0] if isOdd else 0 
+    return (list[n] if isOdd else 0) + oddSum(n - 1)
+
+print(oddSum(len(list) - 1))
