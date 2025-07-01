98d867faeb6f0711ffc62560e44994e1221e5918
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-01 09:54:56 +0530

    bubble sort in python

--- Searching & Sorting - Level 0/Bubble Sort/main.py
+++ Searching & Sorting - Level 0/Bubble Sort/main.py
@@ -1,22 +1,16 @@
+arr = [4,5,1,3,9]
 
-  let arr = [4,5,1,3,9]
-  
-  function bubbleSort(arr){
-    let n = arr.length;
-    for(let i=0; i<n-1; i++) {
-      let isSwapped = false;
-      for(let j=0; j<n-1-i; j++) {
-        if(arr[j]>arr[j+1]) {
-          let temp=arr[j]
-          arr[j]=arr[j+1];
-          arr[j+1]=temp;
-          isSwapped=true;
-        }
-      }
-      if(!isSwapped)
-        break;
-    }
-    return arr;
-  }
-  let result = bubbleSort(arr)
-  console.log("Sorted array",result)
+def binarySort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        isSwapped = False
+        for j in range (n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j+ 1]
+                arr[j + 1] = temp
+                isSwapped = True
+        if not isSwapped : break
+    return arr
+
+print(binarySort(arr))
