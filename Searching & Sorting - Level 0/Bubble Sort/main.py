95efa59ab06b9cebe9556f8f78596af43bf39a63
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-01 09:56:58 +0530

    bubble sort in python a2

--- Searching & Sorting - Level 0/Bubble Sort/main.py
+++ Searching & Sorting - Level 0/Bubble Sort/main.py
@@ -14,3 +14,20 @@ def binarySort(arr):
     return arr
 
 print(binarySort(arr))
+
+
+# swapping approach 2
+
+def binarySort2(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        isSwapped = False
+        for j in range (n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                isSwapped = True
+        if not isSwapped : break
+    return arr
+
+
+print(binarySort2(arr))
