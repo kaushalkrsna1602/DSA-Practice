2c02a13bd73109e9709dd9e001c71ff7c49cf991
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-04 22:30:55 +0530

    Function1 in JS

---
+++ 01_Warm Up/Function, if-else/index.js
@@ -0,0 +1,8 @@
+// Problem Statement:
+// Write a Program that defines a function to calculate the sum of two integers and prints the result. Call this function by passing two integer values.
+
+function sum(a, b) {
+  let add = a + b;
+  console.log(add);
+}
+sum(5, 3);
