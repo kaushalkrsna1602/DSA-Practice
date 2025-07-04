58f2f79a7abed9aea724cab802783fc2fda5bcfd
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-04 22:31:55 +0530

    Function1 in Python

---
+++ 01_Warm Up/Function, if-else/main.py
@@ -0,0 +1,8 @@
+# Problem Statement:
+# Write a Program that defines a function to calculate the sum of two integers and prints the result. Call this function by passing two integer values.
+
+
+def sum(a, b):
+    add = a + b
+    print(add)
+sum(5, 3)
