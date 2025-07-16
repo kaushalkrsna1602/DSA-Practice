6bd98065a6339a67c10f63b57e47a2bd322a12c1
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-16 12:04:24 +0530

    Floyd's Cycle Finding ALgorithm Python

--- Linked-List-Level 0/Linked List Cycle/main.py
+++ Linked-List-Level 0/Linked List Cycle/main.py
@@ -16,3 +16,20 @@ class Solution:
             seen.add(head)
             head = head.next
         return False
+    
+
+
+# Approach 2 : Floyd's Cycle Finding ALgorithm / SLOW AND FAST POINTER APPROACH
+
+class Solution(object):
+    def hasCycle(self, head):
+        if not head or not head.next:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
