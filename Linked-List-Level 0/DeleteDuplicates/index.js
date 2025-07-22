d762b107c62f81d99239dbecdeb05b8ad6ad2b5b
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-22 10:59:08 +0530

    deleteDuplicates [js]

---
+++ Linked-List-Level 0/DeleteDuplicates/index.js
@@ -0,0 +1,27 @@
+/**
+ * 83. Remove Duplicates from Sorted List
+ * 
+ * Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+ * 
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let curr = head;
+    while (curr && curr.next) {
+        if (curr.val === curr.next.val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+    return head;
+};
