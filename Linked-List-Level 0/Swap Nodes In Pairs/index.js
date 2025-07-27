a1283291f3282c33da685c58ea1e6f175fbee079
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-28 02:33:24 +0530

    Swap Nodes in Pairs [js]

--- Linked-List-Level 0/Swap Nodes In Pairs/index.js
+++ Linked-List-Level 0/Swap Nodes In Pairs/index.js
@@ -33,3 +33,26 @@ var swapPairs = function(head) {
     }
     return dummy.next
 };
+
+// Approach 2
+
+var swapPairs2 = function(head) {
+    if (!head || !head.next) return head;
+
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let prev = dummy;
+
+    while (prev.next && prev.next.next) {
+        let first = prev.next;
+        let second = first.next;
+
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+
+        prev = first;
+    }
+    return dummy.next;
+};
+    
