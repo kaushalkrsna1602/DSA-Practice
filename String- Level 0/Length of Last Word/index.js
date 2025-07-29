2ef8df315b9ee9de3e89436aa66e59a875af1263
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-30 01:02:58 +0530

    Length of Last Word [js]

--- String- Level 0/Length of Last Word/index.js
+++ String- Level 0/Length of Last Word/index.js
@@ -40,3 +40,20 @@ function countLastWordLength(s) {
 }
 
 console.log(countLastWordLength("   fly me   to   the moon  "))
+
+
+// Approach 3
+
+var lengthOfLastWord = function(s) {
+    let n = s.length - 1
+    let count = 0;
+    while(n >= 0) {
+        if(s[n] != " ") {
+            ++count
+        }else if (count > 0) {
+            break 
+        }
+        --n
+    }
+    return count
+};
