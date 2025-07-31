dbb29affa4977ad1fb9d32ab67b55662ee2f08d7
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-31 09:51:46 +0530

    Jewels and Stones- O(m*n) - [js]

---
+++ String- Level 0/Jewels and Stones/index.js
@@ -0,0 +1,35 @@
+// 771. Jewels and Stones
+
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+        if(jewels.includes(stones[i])) {
+            ++count
+        }
+    }
+    return count
+};
+
+// Using Two Loops
+
+var numJewelsInStones2 = function(jewels, stones) {
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+        for(let j = 0; j < jewels.length; j++) {
+            if(jewels[j] === stones[i]) {
+                ++count
+                break;
+            }
+        }
+    }
+    return count
+};
