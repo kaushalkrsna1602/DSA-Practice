1a6d5b4c3d80080605f8e16f196162b88067d5d2
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-08-04 04:10:59 +0530

    Find Most Frequent Vowel and Consonant [js]

--- String- Level 0/Find Most Frequent Vowel and Consonant/index.js
+++ String- Level 0/Find Most Frequent Vowel and Consonant/index.js
@@ -78,3 +78,28 @@ var maxFreqSum2 = function (s) {
     }
     return maxConsonant + maxVowels
 }; 
+
+
+// Better code wrting
+var maxFreqSum = function (s) {
+    let map = {}
+    for (i = 0; i < s.length; i++) {
+        map[s[i]] = !map[s[i]] ? 1 : ++map[s[i]]
+    }
+
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let maxVowels = 0
+    let maxConsonant = 0
+    let mapKeys = Object.keys(map)
+
+    for (let i = 0; i < mapKeys.length; i++) {
+        if (vowels.includes(mapKeys[i])) {
+            maxVowels = Math.max(map[mapKeys[i]] ,maxVowels)
+        }
+        else {
+            maxConsonant = Math.max(maxConsonant, map[mapKeys[i]] )
+        }
+    }
+    return maxConsonant + maxVowels
+};   
+
