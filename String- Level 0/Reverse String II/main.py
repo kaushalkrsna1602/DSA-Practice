0bf217866da7894ff2ec1506b39e30dcc86df5fa
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-08-05 20:02:26 +0530

    Reverse String II [py]

---
+++ String- Level 0/Reverse String II/main.py
@@ -0,0 +1,25 @@
+# 541. Reverse String II
+
+# Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+
+# If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+
+ 
+
+# Example 1:
+
+# Input: s = "abcdefg", k = 2
+# Output: "bacdfeg"
+# Example 2:
+
+# Input: s = "abcd", k = 2
+# Output: "bacd"
+ 
+
+
+def reverseStr(s: str, k: int) -> str:
+    s = list(s)
+    for i in range(0, len(s), 2 * k):
+        s[i:i + k] = reversed(s[i:i + k])
+    return ''.join(s)
+      
