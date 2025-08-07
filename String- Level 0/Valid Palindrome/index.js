857dedf7aa28d3fb27f47ce08c565bdf66f0b5b4
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-08-07 07:33:32 +0530

    Valid Palindrome [js]

--- String- Level 0/Valid Palindrome/index.js
+++ String- Level 0/Valid Palindrome/index.js
@@ -1,3 +1,24 @@
+// 125. Valid Palindrome
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+// ASCII Character 
+
 var isPalindrome = function (s) {
 s = s.toLowerCase();
 let filteredString = "";
@@ -13,4 +34,19 @@ for (let i = 0; i < s.length; i++) {
 }
 return filteredString === rev;
 };
-      
+
+// Regex
+
+
+var isPalindrome = function (s) {
+s = s.toLowerCase();
+let filteredString = "";
+let rev = "";
+for (let i = 0; i < s.length; i++) {
+    if(s[i].match(/[a-z0-9]/i)) {
+    filteredString = filteredString + s[i];
+    rev = s[i] + rev;
+    }
+}
+return filteredString === rev;
+};
