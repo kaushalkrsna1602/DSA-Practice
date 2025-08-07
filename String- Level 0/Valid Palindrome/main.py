fdf21bd74e24c5857a0279fc68e5aedf2fa37a18
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-08-07 09:10:40 +0530

    Valid Palindrome- two pointer [py]

--- String- Level 0/Valid Palindrome/main.py
+++ String- Level 0/Valid Palindrome/main.py
@@ -8,3 +8,24 @@ class Solution:
                 filterStr = filterStr + c
                 rev = c + rev
         return rev == filterStr
+    
+
+# Two Pointer Approach
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            elif s[i] == s[j]:
+                i += 1
+                j-= 1
+            else :
+                return False
+        return True
+        
