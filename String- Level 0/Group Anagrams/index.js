98d98ab2ce237771a04284c084d95b2eca27f3d2
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-08-18 11:56:53 +0530

    Group Anagrams [js]

---
+++ String- Level 0/Group Anagrams/index.js
@@ -0,0 +1,38 @@
+// 49. Group Anagrams
+
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Explanation:
+
+// There is no string in strs that can be rearranged to form "bat".
+// The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+// The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+      let map = {};
+  
+      for (let i = 0; i < strs.length; i++) {
+          let sortedStr = strs[i].split("").sort().join(""); 
+  
+          if (!map[sortedStr]) {
+              map[sortedStr] = [strs[i]];
+          } else {
+              map[sortedStr].push(strs[i]);
+          }
+      }
+  
+      return Object.values(map);
+  };
+      
