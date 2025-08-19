0ff5b918b648cbc8bbddfb59e2a0fa0d9fbe6726
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-08-19 23:36:44 +0530

    Group Anagrams [py]

---
+++ String- Level 0/Group Anagrams/main.py
@@ -0,0 +1,16 @@
+def groupAnagrams(strs):
+    anagram_map = {}
+
+    for word in strs:
+        sorted_word = ''.join(sorted(word))  # sort the word alphabetically
+        
+        if sorted_word not in anagram_map:
+            anagram_map[sorted_word] = [word]
+        else:
+            anagram_map[sorted_word].append(word)
+    
+    return list(anagram_map.values())
+
+
+# Example usage:
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
