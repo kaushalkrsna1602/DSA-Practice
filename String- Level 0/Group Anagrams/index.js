1cc8083d6bcabd3da192fe0646284f9105f031d0
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-08-20 12:52:47 +0530

    Group Anagrams [js]

--- String- Level 0/Group Anagrams/index.js
+++ String- Level 0/Group Anagrams/index.js
@@ -36,3 +36,32 @@ var groupAnagrams = function(strs) {
       return Object.values(map);
   };
       
+
+  // Hashed Map
+
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = {}
+    for(let i = 0; i < strs.length; i++) {
+        let freqArr = Array(26).fill(0);
+        let s = strs[i]
+        for (let j = 0; j < s.length; j++) {
+            let index = s[j].charCodeAt() - 'a'.charCodeAt();
+            ++freqArr[index]
+        }
+        let key = ""
+        for(let k = 0; k < 26; k++){
+            key = key + String.fromCharCode(k) + freqArr[k]
+        }
+        if (!map[key]){
+            map[key] = [s]
+        } else {
+            map[key].push(s)
+        }
+    }
+    return [...Object.values(map)]
+};
