93dd9169765132f957556009821203c105dc00f9
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-08-22 21:53:37 +0530

    Group Anagrams [py]

--- String- Level 0/Group Anagrams/main.py
+++ String- Level 0/Group Anagrams/main.py
@@ -14,3 +14,16 @@ def groupAnagrams(strs):
 
 # Example usage:
 print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+
+
+
+  
+def groupAnagrams(strs):
+      anagrams = defaultdict(list)
+  
+      for word in strs:
+          key = ''.join(sorted(word))
+          anagrams[key].append(word)
+  
+      return list(anagrams.values())
