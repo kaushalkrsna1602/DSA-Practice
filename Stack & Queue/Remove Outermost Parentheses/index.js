55340d257f2cfd5836c1bbeac1b69ad4ea2a7a45
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-09-05 10:56:34 +0530

    Remove Outermost Parentheses- without stack [js]

--- Stack & Queue/Remove Outermost Parentheses/index.js
+++ Stack & Queue/Remove Outermost Parentheses/index.js
@@ -16,3 +16,30 @@ var removeOuterParentheses = function(s) {
     }
     return ans;
 };
+
+
+
+
+
+
+
+// without Stack 
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+    let level = 0
+    let ans = ""
+    for(let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            ++level
+            ans += ((level > 1) ? s[i] : "")
+        } else {
+            ans += ((level> 1) ? s[i] : "")
+            --level
+        }
+    }
+    return ans;
+};
